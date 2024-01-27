42abe8ecb54635d745176d96f477fd823fd2eb07
vinojiniParamasivam <vinojini.p@viryaconsulting.com>
2024-01-27 12:02:29 +0530

    Remove duplicate numbers in a list - Java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.println("Hello world!");
         List<Integer> numbers = Arrays.asList(71, 18, 42, 21, 67, 32, 95, 14, 56, 87);
         SeperateOddEven.seperateOddEven(numbers);
-//        List<Integer> numbersDuplicate = Arrays.asList(71, 18, 42, 21, 67, 32, 95, 14, 56, 87,71, 18, 42, 21, 67, 32, 95, 14, 56, 87);
-//        RemoveDuplicates.removeDuplicates(numbersDuplicate);
+        List<Integer> numbersDuplicate = Arrays.asList(71, 18, 42, 21, 67, 32, 95, 14, 56, 87,71, 18, 42, 21, 67, 32, 95, 14, 56, 87);
+        RemoveDuplicates.removeDuplicates(numbersDuplicate);
     }
 }

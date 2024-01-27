6c1eddf88de9dc681ec6a8833082d0131a2b6937
vinojiniParamasivam <vinojini.p@viryaconsulting.com>
2024-01-27 13:28:25 +0530

    Fix issues

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,24 @@
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
-
 public class Main {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(71, 18, 42, 21, 67, 32, 95, 14, 56, 87);
-        SeperateOddEven.seperateOddEven(numbers);
+        System.out.println("Odd number list");
+        numbers.stream().filter(a->a%2==1).forEach(a-> System.out.print(a+" "));
+
+        System.out.println("\nEven number list");
+        numbers.stream().filter(a->a%2==0).forEach(a-> System.out.print(a+" "));
 
         List<Integer> numbersDuplicate = Arrays.asList(71, 18, 42, 21, 67, 32, 95, 14, 56, 87, 71, 18, 42, 21, 67, 32, 95, 14, 56, 87);
-        numbersDuplicate.stream().distinct().forEach(a-> System.out.println(a+" "));
+
+        System.out.println("\nDuplicates removed list");
+        numbersDuplicate.stream().distinct().forEach(a-> System.out.print(a+" "));
 
         System.out.println("\nList sorted in natural order");
         numbers.stream().sorted().forEach(a -> System.out.print(a + " "));
 
-        System.out.println("List sorted in reverse order");
+        System.out.println("\nList sorted in reverse order");
         numbers.stream().sorted(Comparator.reverseOrder()).forEach(a -> System.out.print(a + " "));
     }
 }

ab1c0af50121e942315d35c59d042ce93b30b13d
vinojiniParamasivam <vinojini.p@viryaconsulting.com>
2024-01-27 13:20:59 +0530

    Sort numbers in ascending and descending orders

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         List<Integer> numbers = Arrays.asList(71, 18, 42, 21, 67, 32, 95, 14, 56, 87);
         SeperateOddEven.seperateOddEven(numbers);
-        List<Integer> numbersDuplicate = Arrays.asList(71, 18, 42, 21, 67, 32, 95, 14, 56, 87,71, 18, 42, 21, 67, 32, 95, 14, 56, 87);
-        RemoveDuplicates.removeDuplicates(numbersDuplicate);
+
+        List<Integer> numbersDuplicate = Arrays.asList(71, 18, 42, 21, 67, 32, 95, 14, 56, 87, 71, 18, 42, 21, 67, 32, 95, 14, 56, 87);
+        numbersDuplicate.stream().distinct().forEach(a-> System.out.println(a+" "));
+
+        System.out.println("\nList sorted in natural order");
+        numbers.stream().sorted().forEach(a -> System.out.print(a + " "));
+
+        System.out.println("List sorted in reverse order");
+        numbers.stream().sorted(Comparator.reverseOrder()).forEach(a -> System.out.print(a + " "));
     }
 }
